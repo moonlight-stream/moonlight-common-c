48856fed88fe7785b27319c0af977306fdf66f2e
Iwan Timmer <irtimmer@gmail.com>
2017-01-24 16:58:06 +0100

    Initial implementation for single FEC packet

--- src/RtpFecQueue.c
+++ src/RtpFecQueue.c
@@ -1,10 +1,12 @@
 #include "Limelight-internal.h"
 #include "RtpFecQueue.h"
+#include "rs.h"
 
 #define ushort(x) (unsigned short) ((x) % (UINT16_MAX+1))
 #define isBefore(x, y) ushort((x) - (y)) > (UINT16_MAX/2)
 
 void RtpfInitializeQueue(PRTP_FEC_QUEUE queue) {
+    reed_solomon_init();
     memset(queue, 0, sizeof(*queue));
     queue->nextRtpSequenceNumber = UINT16_MAX;
     
@@ -98,7 +100,93 @@ static void repairPackets(PRTP_FEC_QUEUE queue) {
         return;
     }
     
-    //TODO repair packets and add through queueRecoveredPacket()
+    reed_solomon* rs = reed_solomon_new(queue->bufferDataPackets, 1);
+
+    int* missing = malloc(missingPackets * sizeof(int));
+    unsigned char** packets = malloc(totalPackets * sizeof(unsigned char*));
+    unsigned char* marks = malloc(totalPackets * sizeof(unsigned char));
+    if (rs == NULL || missing == NULL || packets == NULL || marks == NULL)
+        goto cleanup;
+
+    memset(marks, 1, sizeof(char) * (totalPackets));
+    
+    int receiveSize = StreamConfig.packetSize + MAX_RTP_HEADER_SIZE;
+    int packetBufferSize = receiveSize + sizeof(int) + sizeof(RTPFEC_QUEUE_ENTRY);
+
+    PRTPFEC_QUEUE_ENTRY entry = queue->bufferHead;
+    while (entry != NULL) {
+        int index = ushort(entry->packet->sequenceNumber - queue->bufferLowestSequenceNumber);
+        packets[index] = (unsigned char*) entry->packet;
+        marks[index] = 0;
+
+        int size;
+        memcpy(&size, &packets[index][StreamConfig.packetSize + MAX_RTP_HEADER_SIZE], sizeof(int));
+        
+        //Set padding to zero
+        if (size < receiveSize) {
+            memset(&packets[index][size], 0, receiveSize - size);
+        }
+
+        entry = entry->next;
+    }
+
+    int i;
+    int ret = -1;
+    for (i = 0; i < totalPackets; i++) {
+        if (marks[i]) {
+            packets[i] = malloc(packetBufferSize);
+            if (packets[i] == NULL) {
+                goto cleanup_packets;
+            }
+        }
+    }
+    
+    ret = reed_solomon_reconstruct(rs, packets, marks, totalPackets, receiveSize);
+
+    cleanup_packets:
+    for (i = 0; i < totalPackets; i++) {
+        if (marks[i]) {
+            if (ret == 0) {
+                PRTPFEC_QUEUE_ENTRY queueEntry = (PRTPFEC_QUEUE_ENTRY)&packets[i][receiveSize + sizeof(int)];
+                PRTP_PACKET rtpPacket = (PRTP_PACKET) packets[i];
+                rtpPacket->sequenceNumber = ushort(i + queue->bufferLowestSequenceNumber);
+                rtpPacket->header = queue->bufferHead->packet->header;
+                
+                int dataOffset = sizeof(*rtpPacket);
+                if (rtpPacket->header & FLAG_EXTENSION) {
+                    dataOffset += 4; // 2 additional fields
+                }
+
+                PNV_VIDEO_PACKET nvPacket = (PNV_VIDEO_PACKET)(((char*)rtpPacket) + dataOffset);
+                nvPacket->frameIndex = queue->currentFrameNumber;
+                
+                //Remove padding of generated packet
+                int size = StreamConfig.packetSize + dataOffset;
+                if (rtpPacket->sequenceNumber == ushort(queue->bufferLowestSequenceNumber + queue->bufferDataPackets - 1)) {
+                    while (packets[i][size-1] == 0) {
+                        size--;
+                    }
+                }
+
+                memcpy(&packets[i][receiveSize], &size, sizeof(int));
+                queuePacket(queue, queueEntry, 0, rtpPacket);
+            } else if (packets[i] != NULL) {
+                free(packets[i]);
+            }
+        }
+    }
+
+    cleanup:
+    reed_solomon_release(rs);
+
+    if (missing != NULL)
+        free(missing);
+
+    if (packets != NULL)
+        free(packets);
+
+    if (marks != NULL)
+        free(marks);
 }
 
 static void removeEntry(PRTP_FEC_QUEUE queue, PRTPFEC_QUEUE_ENTRY entry) {
