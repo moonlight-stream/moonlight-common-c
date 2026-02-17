7e4831dfcfc98d3b17df44b9e262c2372d5bf0c7
Andy Grundman <andy@hybridized.org>
2026-02-17 16:35:38 -0500

    nanors: don't use NEON asm with MSVC ARM64

--- nanors/deps/obl/autoshim.h
+++ nanors/deps/obl/autoshim.h
@@ -7,7 +7,9 @@
 #if defined(__SSSE3__) || (defined(_MSC_VER) && defined(_M_X64) && !defined(_M_ARM64))
 #define OBLAS_SSE3
 #else
-#if defined(__aarch64__) || (defined(_MSC_VER) && defined(_M_ARM64))
+// This doesn't build right with MSVC
+// #if defined(__aarch64__) || (defined(_MSC_VER) && defined(_M_ARM64))
+#if defined(__aarch64__)
 #define OBLAS_NEON
 #endif
 #endif
