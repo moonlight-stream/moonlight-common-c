3872285d8cd6fe7d32b8191a8a1cd7d5de9f133f
Cameron Gutman <aicommander@gmail.com>
2026-02-19 20:03:20 -0600

    Fix incorrectly enabling SSSE3 on all x64 Windows platforms

--- nanors/deps/obl/autoshim.h
+++ nanors/deps/obl/autoshim.h
@@ -4,7 +4,7 @@
 #if defined(__AVX2__)
 #define OBLAS_AVX2
 #else
-#if defined(__SSSE3__) || (defined(_MSC_VER) && defined(_M_X64) && !defined(_M_ARM64))
+#if defined(__SSSE3__)
 #define OBLAS_SSE3
 #endif
 #endif
